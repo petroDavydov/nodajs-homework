ea1e92b4832d03b1ddef2792f7bbbda2ab2302f0
Petro Davydov <78567740+petroDavydov@users.noreply.github.com>
2022-07-31 00:24:11 +0300

    remember fs, event loop, wF,rF etc

--- argv.js
+++ argv.js
@@ -1,6 +1,40 @@
-const fs = require('node:fs')
-const argv = process.argv
-const { arch } = require('node:process')
+// const fs = require('node:fs')
+// const argv = process.argv
+// const { arch } = require('node:process')
+// const { argv } = require('node:process')
 
-console.log(argv)
-console.log(`This process architecture ${arch}`)
+// argv.forEach((val, index) => {
+//     console.log(`${val} - ${index}`)
+// })
+
+// console.log(argv)
+// console.log(`This process architecture ${arch}`)
+
+// ===========================
+
+const fs = require('node:fs/promises')
+
+const argv = process.argv
+
+const main = async () => {
+    let data
+    try {
+        data = await (fs.readFile('data.json', 'utf-8'))
+    } catch {
+        data = '[]'
+    }
+    const content = JSON.parse(data)
+    if (argv[2] === '--list') {
+        console.table(content)
+    } else if (argv[2] === void 0) {
+        console.log('Unknown command')
+    } else {
+        const name = argv[2]
+        const age = argv[3]
+        content.push({ name, age })
+        await fs.writeFile('data.json', JSON.stringify(content, null, 2))
+    }
+
+}
+
+main()
---
+++ argv.mjs
@@ -0,0 +1,26 @@
+
+import fs from 'node:fs/promises'
+
+const argv = process.argv
+
+
+let data
+try {
+    data = await (fs.readFile('data.json', 'utf-8'))
+} catch {
+    data = '[]'
+}
+const content = JSON.parse(data)
+if (argv[2] === '--list') {
+    console.table(content)
+} else if (argv[2] === void 0) {
+    console.log('Unknown command')
+} else {
+    const name = argv[2]
+    const age = argv[3]
+    content.push({ name, age })
+    await fs.writeFile('data.json', JSON.stringify(content, null, 2))
+}
+
+
+
